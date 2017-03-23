fd552492e1b7300c62e5d61a7f39e28d41762b05
Maggie Rose <maggie.rose.g@gmail.com>
2017-03-23 15:55:31 -0600

    Add account activation

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 20170323160617) do
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
     t.string   "email"
     t.datetime "created_at",                        null: false
@@ -23,7 +23,7 @@ ActiveRecord::Schema.define(version: 20170323160617) do
     t.string   "activation_digest"
     t.boolean  "activated",         default: false
     t.datetime "activated_at"
-    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
   end
 
 end
